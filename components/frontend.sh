e21f3fa81fbd56fdf682f7b07bf201c66bc972cc
tirumalaraju <sireesha@zelarsoft.com>
2021-05-06 22:05:56 +0530

    m

--- components/frontend.sh
+++ components/frontend.sh
@@ -15,7 +15,7 @@ apt install npm -y &>>$LOG
 Stat $?
 
 Head "Change The Path"
-sed -i 's#/path/to/shell-script#/path/to/etc/nginx/sites-available#g' && sed -i 's|/var/www/html|/var/www/html/frontend|g' default
+sed -i 's#/path/to/shell-script#/path/to/etc/nginx/sites-available#g' 
 Stat $?
 
 
