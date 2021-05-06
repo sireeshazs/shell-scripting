557199f91a8e7f491ac8a0161846722af3c0f261
tirumalaraju <sireesha@zelarsoft.com>
2021-05-06 20:28:10 +0530

    n

--- components/frontend.sh
+++ components/frontend.sh
@@ -10,12 +10,11 @@ Head "Installing Nginx"
 apt install nginx -y &>>$LOG
 Stat $?
 
-
-DOWNLOAD_COMPONENT
-
-Head "Remove Default Configuration"
-rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+Head "Installing Npm"
+apt install npm -y &>>$LOG
 Stat $?
+
+
 
 
 
