f3e7903eaba16d39ec64f6baa2015b8715569216
tirumalaraju <sireesha@zelarsoft.com>
2021-05-06 17:05:12 +0530

    n

--- components/frontend.sh
+++ components/frontend.sh
@@ -9,3 +9,15 @@ OS_PREREQ
 Head "Installing Nginx"
 apt install nginx -y &>>$LOG
 Stat $?
+
+
+DOWNLOAD_COMPONENT
+
+Head "Remove Default Configuration"
+rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+Stat $?
+
+
+
+
+
