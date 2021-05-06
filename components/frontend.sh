75779b36cc2d6242d8f674bbb1f52ed02dbb9ede
tirumalaraju <sireesha@zelarsoft.com>
2021-05-06 20:45:18 +0530

    N

--- components/frontend.sh
+++ components/frontend.sh
@@ -14,6 +14,15 @@ Head "Installing Npm"
 apt install npm -y &>>$LOG
 Stat $?
 
+Head "Change The Path"
+cd /var/www/html && mkdir vue & cd /var/www/html/vue
+Stat $?
+
+DOWNLOAD_COMPONENTS
+
+
+
+
 
 
 
