edd84a3a325c10e50d432c7621d298a7100a8d85
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 22:32:21 +0530

    n

--- components/users.sh
+++ components/users.sh
@@ -16,6 +16,6 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users && cd users && mvn clean package &>>$LOG 
+cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG && mv target/users-api-0.0.1.jar users.jar
 Stat $?
 
