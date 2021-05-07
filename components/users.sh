1683c49ee730b7fbd3f57c2b89247457776c6c9d
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 18:56:21 +0530

    m

--- components/users.sh
+++ components/users.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-echo users
+
+source components/common.sh
+
+DOMAIN="zsdevops01.online"
+
+OS_PREREQ
+
+Head "Installing Maven & java"
+apt install maven -y &>>$LOG
+apt install openjdk-8-dk &>>$LOG
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+cd users
+
+Head "Setup SystemD Service"
+mv systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable login &>>$LOG
+Stat $?
+
+
