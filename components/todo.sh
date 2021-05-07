09ff7763338d3103b8def820e9ee2c62fdb94b13
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 23:20:49 +0530

    n

--- components/todo.sh
+++ components/todo.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo todo
+
+source components/common.sh
+
+OS_PREREQ
+
+Head "Installing npm"
+apt install npm -y &>>$LOG
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/ubuntu && rm -rf todo && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo && cd /home/ubuntu/todo && npm install &>>$LOG
+Stat $?
+
+Head "Setup SystemD Service"
+mv /systemd.service /etc/systemd/system/todo.service && systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
+Stat $?
