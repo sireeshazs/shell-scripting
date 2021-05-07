e14b6e129641902c418c618a920bbd016ea1209b
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 21:38:19 +0530

    l\

--- components/users.sh
+++ components/users.sh
@@ -2,26 +2,29 @@
 
 source components/common.sh
 
-DOMAIN="zsdevops01.online"
-
 OS_PREREQ
 
-Head "Installing Java"
-apt install openjdk-8-jdk -y &>>$LOG
+Head "Installing java"
+sudo apt install openjdk-8-jdk &>>$LOG
 Stat $?
 
-Head "Installing Maven"
+Head "Installing maven"
 apt install maven -y &>>$LOG
 Stat $?
+
 
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users && cd /home/ubuntu/users &&  mvn clean package  &>>$LOG
+cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG 
 Stat $?
+
+Head "Update EndPoints in Service File"
+sed -i -e "s/user_endpoint/login.venu6776.tk/" /home/ubuntu/users/systemd.service
+Stat $?
+
 
 Head "Setup SystemD Service"
 mv /home/ubuntu/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users &>>$LOG
 Stat $?
-
 
