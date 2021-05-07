fca23c804f00054b1876a6101aaa73d8dcba3174
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 12:50:04 +0530

    m

--- components/login.sh
+++ components/login.sh
@@ -15,3 +15,8 @@ DOWNLOAD_COMPONENT
 Head "Change The Path"
 cd login && export GOPATH=/go
 Stat $?
+
+
+Head "Setup SystemD Service"
+mv systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
+Stat $?
