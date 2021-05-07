ea6cf5e99b5fae92e32fc5d0473a1fc80bbf018c
tirumalaraju <sireesha@zelarsoft.com>
2021-05-07 20:49:02 +0530

    m

--- components/login.sh
+++ components/login.sh
@@ -10,16 +10,9 @@ Head "Installing Golang"
 apt install golang -y &>>$LOG
 Stat $?
 
-cd /home/ubuntu
-
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
 cd /home/ubuntu && rm -rf login && unzip -o /tmp/login.zip &>>$LOG && mv login-main login && cd /home/ubuntu/login && export GOPATH=/go && go build
-Stat $?
-
-
-Head "Setup SystemD Service"
-mv /systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
 Stat $?
 
