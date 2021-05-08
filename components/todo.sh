c45f25029f89607f6a1c73453b6a336f3355933e
tirumalaraju <sireesha@zelarsoft.com>
2021-05-08 11:33:34 +0530

    m

--- components/todo.sh
+++ components/todo.sh
@@ -4,20 +4,22 @@ source components/common.sh
 
 OS_PREREQ
 
+
 Head "Installing npm"
 apt install npm -y &>>$LOG
 Stat $?
 
+
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf todo && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo && cd /home/ubuntu/todo && npm install &>>$LOG
+cd /home/ubuntu && rm -rf todo && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo  && cd todo && npm install &>>$LOG
 Stat $?
-
 
 Head "Update EndPoints in Service File"
 sed -i -e "s/user_endpoint/login.venu6776.tk/" /home/ubuntu/todo/systemd.service
 Stat $?
+
 
 Head "Setup SystemD Service"
 mv /home/ubuntu/todo/systemd.service /etc/systemd/system/todo.service && systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
