06a46f761b72077c31361ee2a096f234c396181d
tirumalaraju <sireesha@zelarsoft.com>
2021-05-10 12:48:18 +0530

    m

--- components/frontend.sh
+++ components/frontend.sh
@@ -8,6 +8,7 @@ OS_PREREQ
 
 Head "Installing Nginx"
 apt install nginx -y &>>$LOG
+systemctl restart nginx
 Stat $?
 
 Head "Installing Npm"
@@ -21,8 +22,25 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf frontend && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main frontend && cd /home/ubuntu/frontend && npm run build &>>$LOG
+cd /var/www/html && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
+
+Head "update end points in service file"
+cd /var/www/html/vue/frontend
+export AUTH_API_ADDRESS=http://login.venu6766.tk:8080
+export TODOS_API_ADDRESS=http://todo.venu6766.tk:8080
+Stat $?
+
+Head "update frontend configuration"
+cd /var/www/html/vue/frontend  && sudo npm install --unsafe-perm sass sass-loader node-sass wepy-compiler-sass &>>$LOG && npm run build &>>$LOG 
+Stat $?
+
+head "Start Npm service"
+npm start 
+Stat $?
+
+
+
 
 
 
