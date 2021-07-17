f0711c11cba934c909f74160f33f3c9d6b50d0ba
sireeshazs <82812425+sireeshazs@users.noreply.github.com>
2021-07-17 15:47:29 +0530

    Update frontend.sh

--- components/frontend.sh
+++ components/frontend.sh
@@ -15,28 +15,27 @@ Head "Installing Npm"
 apt install npm -y &>>$LOG
 Stat $?
 
-Head "Change The Path"
-cd /etc/nginx/sites-available && sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' default 
-Stat $?
-
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /var/www/html && rm -rf vue && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip && rm -rf frontend && mv frontend-main frontend && cd frontend
+cd /var/www/html && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
 
-Head "update end points in service file"
-cd /var/www/html/vue/frontend
-export AUTH_API_ADDRESS=http://login.${DOMAIN}:8080
-export TODOS_API_ADDRESS=http://todo.${DOMAIN}:8080
-Stat $?
 
 Head "update frontend configuration"
-cd /var/www/html/vue/frontend  && sudo npm install &>>$LOG && npm run build &>>$LOG 
+cd /var/www/html/frontend  && sudo npm install &>>$LOG && npm run build &>>$LOG 
 Stat $?
 
-Head "Start Npm service"
-npm start 
+
+Head "Update Nginx Configuration"
+mv todo.conf /etc/nginx/sites-enabled/todo.conf
+for comp in login todo ; do
+  sed -i -e "/$comp/ s/localhost/${comp}.${DOMAIN}/" /etc/nginx/sites-enabled/todo.conf
+done
+Stat $?
+
+Head "ReStart Nginx service"
+systemctl restart nginx
 Stat $?
 
 
