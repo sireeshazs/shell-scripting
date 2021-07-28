56efb9e4d480e086cd2ee553547cda2ac3f7fc51
tirumalaraju <sireesha@zelarsoft.com>
2021-07-28 12:45:48 +0530

    k

--- components/frontend.sh
+++ components/frontend.sh
@@ -16,10 +16,6 @@ Stat $?
 
 DOWNLOAD_COMPONENT
 
-Head "Remove Default Configuration"
-rm -rf /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
-Stat $?
-
 Head "Extract Downloaded Archive"
 cd /var/www/html && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
